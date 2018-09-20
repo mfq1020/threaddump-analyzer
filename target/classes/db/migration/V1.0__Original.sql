143683894477d9bfa4f374740d6c9760088be2f3
LHearen <lhearen@gmail.com>
2018-09-20 14:45:12 +0800

    add gitignore

--- target/classes/db/migration/V1.0__Original.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS s3_info;
-CREATE TABLE s3_info (
-  id bigint(20) AUTO_INCREMENT,
-  access_key varchar(128) NOT NULL,
-  secret_key varchar(128) NOT NULL,
-  PRIMARY KEY (id)
-);
